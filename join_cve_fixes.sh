8d64dc25974b2ca86cea6ec63ef996da228f4ea1
Kristiina <kristiina.ra@gmail.com>
2022-05-22 11:41:06 -0400

    Added filtering

--- join_cve_fixes.sh
+++ join_cve_fixes.sh
@@ -17,3 +17,4 @@
 
 sort data/cvefixes.csv -t \; -k 2 > cvefixes-sorted.csv
 join cvefixes-sorted.csv  -1 2 -2 1 -t \; <( zcat /da5_data/basemaps/gz/c2fbbFullU0.s ) >> results/join2.csv
+awk -F\; '$3 == $6' results/join2.csv > results/filtered.csv
