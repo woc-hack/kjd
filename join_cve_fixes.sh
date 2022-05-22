82cf420e46270b17cbdf2733d8ba5d313a8049e5
Kristiina <kristiina.ra@gmail.com>
2022-05-22 12:38:58 -0400

    Added for loop over all c2fbb files

--- join_cve_fixes.sh
+++ join_cve_fixes.sh
@@ -16,5 +16,9 @@
 #------------------------------------------------------------------------
 
 sort data/cvefixes.csv -t \; -k 2 > cvefixes-sorted.csv
-join cvefixes-sorted.csv  -1 2 -2 1 -t \; <( zcat /da5_data/basemaps/gz/c2fbbFullU0.s ) >> results/join2.csv
-awk -F\; '$3 == $6' results/join2.csv > results/filtered.csv
+
+for i in {0..127}
+   do join cvefixes-sorted.csv  -1 2 -2 1 -t \; <( zcat /da5_data/basemaps/gz/c2fbbFullU$i.s ) >> results/join$i.csv
+   awk -F\; '$3 == $6' results/join$i.csv > results/filtered$i.csv
+done
+
